c5c8d9d1c4092498b946e090ed19c2beee54e2fe
Aniketbaghel09 <26aniket09@gmail.com>
2024-04-15 16:57:39 +0530

    oky

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import WeatherApp from './Components/weatherApp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+     <React.Fragment>
+     <div className="container">
+       <WeatherApp />
+     </div>
+     <div className="footer-info">
+       <a href="">
+         Download Source Code
+       </a>{" "}
+       | Developed by{" "}
+       <a target="_blank" href="https://www.linkedin.com/in/aniket-baghel-aa3390215">
+         Aniket Baghel
+       </a>
+      
+     </div>
+   </React.Fragment>
   );
 }
 
